7df6581879a939cadb1819f27fc73ad3ee5ffb7f
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2023-12-20 18:02:03 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='m-8 font-bold text-3xl text-blue-300'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/types/global.types.ts
@@ -0,0 +1,5 @@
+export interface PhotoGallery {
+    id: string | number,
+    slug: string,
+    isSelected: boolean
+}
